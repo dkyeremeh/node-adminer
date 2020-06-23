6126083ea9c8cdb8b2f35405df0c594336a5e796
Desmond Kyeremeh <dkyeremeh@gozego.com>
2020-06-23 10:52:40 +0000

    Added install script

---
+++ install.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const fs = require('fs-extra');
+const download = require('download');
+const adminerSrc = 'https://github.com/vrana/adminer/releases/download/v4.7.7/adminer-4.7.7-en.php';
+const themeUrl =
+    'https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css';
+
+fs.mkdirp("public");
+
+// Download Adminer
+download(adminerSrc).pipe(fs.createWriteStream('public/index.php'));
+
+// Download theme
+download(themeUrl).pipe(fs.createWriteStream('public/adminer.css'));
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const phpServer = require('php-server');
- 
+const path = require('path');
+
 (async () => {
     const server = await phpServer({
         port: process.env.PORT || 8080,
-        base: __dirname
+        base: path.resolve(__dirname, 'public')
     });
-    console.log(`PHP server running at ${server.url}`)
+    console.log(`PHP server running at ${server.url}`);
 })();
