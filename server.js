2560f323bc7dfb9d422801064192fb7a275805e1
Desmond Kyeremeh <dkyeremeh@gozego.com>
2020-06-23 12:51:12 +0000

    open browser on run

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const phpServer = require('php-server');
 const path = require('path');
+const exec = require('child_process').exec
+const start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
 
 (async () => {
     const server = await phpServer({
@@ -7,4 +9,5 @@ const path = require('path');
         base: path.resolve(__dirname, 'public')
     });
     console.log(`PHP server running at ${server.url}`);
+    exec(`${start} ${server.url}`);
 })();
