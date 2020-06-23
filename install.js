3474faf7308f23c25d31a41e7a429aef01e017da
Desmond Kyeremeh <dkyeremeh@gozego.com>
2020-06-23 11:55:08 +0000

    added homepage and git repo

--- install.js
+++ install.js
@@ -2,14 +2,20 @@
 
 const fs = require('fs-extra');
 const download = require('download');
-const adminerSrc = 'https://github.com/vrana/adminer/releases/download/v4.7.7/adminer-4.7.7-en.php';
+const path = require('path');
+const adminerSrc =
+    'https://github.com/vrana/adminer/releases/download/v4.7.7/adminer-4.7.7-en.php';
 const themeUrl =
     'https://raw.githubusercontent.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css';
 
-fs.mkdirp("public");
+fs.mkdirp('public');
 
 // Download Adminer
-download(adminerSrc).pipe(fs.createWriteStream('public/index.php'));
+download(adminerSrc).pipe(
+    fs.createWriteStream(path.resolve(__dirname, 'public/index.php'))
+);
 
 // Download theme
-download(themeUrl).pipe(fs.createWriteStream('public/adminer.css'));
+download(themeUrl).pipe(
+    fs.createWriteStream(path.resolve(__dirname, 'public/adminer.css'))
+);
